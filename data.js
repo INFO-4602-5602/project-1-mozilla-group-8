74234960f181dc451e79cd8ad070e57922118c77
anma9990 <38304341+anma9990@users.noreply.github.com>
2019-10-31 17:08:58 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,28 @@
+priority_1 = {
+            "children": [{"Name":"Price","Count":45444,"Priority":1},
+                {"Name":"Features","Count":40239,"Priority":1},
+                {"Name":"Safety","Count":5305,"Priority":1},
+                {"Name":"Security","Count":8051,"Priority":1},
+                {"Name":"Privacy","Count":8442,"Priority":1},
+                {"Name":"Reliability","Count":13337,"Priority":1},
+                {"Name":"User Reviews","Count":3792,"Priority":1},
+                {"Name":"Expert Recommendation","Count":6185,"Priority":1},
+                {"Name":"Friend/Family Recommendation","Count":3002,"Priority":1},
+                {"Name":"Convenience","Count":21600,"Priority":1}]
+        };
+
+priority_2 = {
+            "children": [{"Name":"Price","Count":24029,"Priority":2},
+                {"Name":"Features","Count":35723,"Priority":2},
+                {"Name":"Safety","Count":11363,"Priority":2},
+                {"Name":"Security","Count":16508,"Priority":2},
+                {"Name":"Privacy","Count":12852,"Priority":2},
+                {"Name":"Reliability","Count":23296,"Priority":2},
+                {"Name":"User Reviews","Count":7509,"Priority":2},
+                {"Name":"Expert Recommendation","Count":7382,"Priority":2},
+                {"Name":"Friend/Family Recommendation","Count":3598,"Priority":2},
+                {"Name":"Convenience","Count":11848,"Priority":2}]
+        };
+
+// will complete later!
+// html only does one data set at a time
