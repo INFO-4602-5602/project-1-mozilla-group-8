d60e4c32ceceadd2168b8d4aacad29d81285c676
anma9990 <38304341+anma9990@users.noreply.github.com>
2019-11-02 11:35:01 -0600

    Add files via upload

---
+++ categorical_data.js
@@ -0,0 +1,133 @@
+// Data for categorical visualization (categorical.html)
+// Each dataset holds information for one bubble set
+
+priority_1 = {
+            "children": [{"Name":"Price","Count":45444,"Priority":1},
+                {"Name":"Features","Count":40239,"Priority":1},
+                {"Name":"Safety","Count":5305,"Priority":1},
+                {"Name":"Security","Count":8051,"Priority":1},
+                {"Name":"Privacy","Count":8442,"Priority":1},
+                {"Name":"Reliability","Count":13337,"Priority":1},
+                {"Name":"User Reviews","Count":3792,"Priority":1},
+                {"Name":"Expert Recommendation","Count":6185,"Priority":1},
+                {"Name":"Friend/Family Recommendation","Count":3002,"Priority":1},
+                {"Name":"Convenience","Count":21600,"Priority":1}]
+        };
+
+priority_2 = {
+            "children": [{"Name":"Price","Count":24029,"Priority":2},
+                {"Name":"Features","Count":35723,"Priority":2},
+                {"Name":"Safety","Count":11363,"Priority":2},
+                {"Name":"Security","Count":16508,"Priority":2},
+                {"Name":"Privacy","Count":12852,"Priority":2},
+                {"Name":"Reliability","Count":23296,"Priority":2},
+                {"Name":"User Reviews","Count":7509,"Priority":2},
+                {"Name":"Expert Recommendation","Count":7382,"Priority":2},
+                {"Name":"Friend/Family Recommendation","Count":3598,"Priority":2},
+                {"Name":"Convenience","Count":11848,"Priority":2}]
+        };
+
+priority_3 = {
+            "children": [{"Name":"Price","Count":17067,"Priority":3},
+                {"Name":"Features","Count":18568,"Priority":3},
+                {"Name":"Safety","Count":15656,"Priority":3},
+                {"Name":"Security","Count":19336,"Priority":3},
+                {"Name":"Privacy","Count":18326,"Priority":3},
+                {"Name":"Reliability","Count":25747,"Priority":3},
+                {"Name":"User Reviews","Count":10879,"Priority":3},
+                {"Name":"Expert Recommendation","Count":9897,"Priority":3},
+                {"Name":"Friend/Family Recommendation","Count":4922,"Priority":3},
+                {"Name":"Convenience","Count":12373,"Priority":3}]
+        };
+
+priority_4 = {
+            "children": [{"Name":"Price","Count":14568,"Priority":4},
+                {"Name":"Features","Count":13595,"Priority":4},
+                {"Name":"Safety","Count":15868,"Priority":4},
+                {"Name":"Security","Count":19307,"Priority":4},
+                {"Name":"Privacy","Count":19253,"Priority":4},
+                {"Name":"Reliability","Count":22741,"Priority":4},
+                {"Name":"User Reviews","Count":12315,"Priority":4},
+                {"Name":"Expert Recommendation","Count":11657,"Priority":4},
+                {"Name":"Friend/Family Recommendation","Count":6235,"Priority":4},
+                {"Name":"Convenience","Count":12956,"Priority":4}]
+        };
+
+priority_5 = {
+            "children": [{"Name":"Price","Count":12129,"Priority":5},
+                {"Name":"Features","Count":11848,"Priority":5},
+                {"Name":"Safety","Count":18061,"Priority":5},
+                {"Name":"Security","Count":18498,"Priority":5},
+                {"Name":"Privacy","Count":18694,"Priority":5},
+                {"Name":"Reliability","Count":18214,"Priority":5},
+                {"Name":"User Reviews","Count":11880,"Priority":5},
+                {"Name":"Expert Recommendation","Count":12265,"Priority":5},
+                {"Name":"Friend/Family Recommendation","Count":7913,"Priority":5},
+                {"Name":"Convenience","Count":12850,"Priority":5}]
+        };
+
+priority_6 = {
+            "children": [{"Name":"Price","Count":11061,"Priority":6},
+                {"Name":"Features","Count":10328,"Priority":6},
+                {"Name":"Safety","Count":19287,"Priority":6},
+                {"Name":"Security","Count":16959,"Priority":6},
+                {"Name":"Privacy","Count":17088,"Priority":6},
+                {"Name":"Reliability","Count":15381,"Priority":6},
+                {"Name":"User Reviews","Count":11638,"Priority":6},
+                {"Name":"Expert Recommendation","Count":11690,"Priority":6},
+                {"Name":"Friend/Family Recommendation","Count":8777,"Priority":6},
+                {"Name":"Convenience","Count":13893,"Priority":6}]
+        };
+
+priority_7 = {
+            "children": [{"Name":"Price","Count":9879,"Priority":7},
+                {"Name":"Features","Count":7299,"Priority":7},
+                {"Name":"Safety","Count":17694,"Priority":7},
+                {"Name":"Security","Count":14441,"Priority":7},
+                {"Name":"Privacy","Count":16222,"Priority":7},
+                {"Name":"Reliability","Count":10953,"Priority":7},
+                {"Name":"User Reviews","Count":14085,"Priority":7},
+                {"Name":"Expert Recommendation","Count":14116,"Priority":7},
+                {"Name":"Friend/Family Recommendation","Count":11061,"Priority":7},
+                {"Name":"Convenience","Count":15617,"Priority":7}]
+        };
+
+priority_8 = {
+            "children": [{"Name":"Price","Count":5652,"Priority":8},
+                {"Name":"Features","Count":4044,"Priority":8},
+                {"Name":"Safety","Count":14504,"Priority":8},
+                {"Name":"Security","Count":12049,"Priority":8},
+                {"Name":"Privacy","Count":13420,"Priority":8},
+                {"Name":"Reliability","Count":6547,"Priority":8},
+                {"Name":"User Reviews","Count":20370,"Priority":8},
+                {"Name":"Expert Recommendation","Count":22490,"Priority":8},
+                {"Name":"Friend/Family Recommendation","Count":17598,"Priority":8},
+                {"Name":"Convenience","Count":11413,"Priority":8}]
+        };
+
+priority_9 = {
+            "children": [{"Name":"Price","Count":4341,"Priority":9},
+                {"Name":"Features","Count":2886,"Priority":9},
+                {"Name":"Safety","Count":10921,"Priority":9},
+                {"Name":"Security","Count":9323,"Priority":9},
+                {"Name":"Privacy","Count":9757,"Priority":9},
+                {"Name":"Reliability","Count":4598,"Priority":9},
+                {"Name":"User Reviews","Count":22726,"Priority":9},
+                {"Name":"Expert Recommendation","Count":22188,"Priority":9},
+                {"Name":"Friend/Family Recommendation","Count":26923,"Priority":9},
+                {"Name":"Convenience","Count":12490,"Priority":9}]
+        };
+
+priority_10 = {
+            "children": [{"Name":"Price","Count":6167,"Priority":10},
+                {"Name":"Features","Count":2102,"Priority":10},
+                {"Name":"Safety","Count":8191,"Priority":10},
+                {"Name":"Security","Count":4676,"Priority":10},
+                {"Name":"Privacy","Count":6509,"Priority":10},
+                {"Name":"Reliability","Count":2335,"Priority":10},
+                {"Name":"User Reviews","Count":20026,"Priority":10},
+                {"Name":"Expert Recommendation","Count":17623,"Priority":10},
+                {"Name":"Friend/Family Recommendation","Count":40829,"Priority":10},
+                {"Name":"Convenience","Count":15063,"Priority":10}]
+        };
+
